35fc79051720e1952f9ebd36273e9ec8bd7c4df2
tufan35 <34610153+tufan35@users.noreply.github.com>
2023-08-29 18:19:08 +0300

    Add project files.

---
+++ RabbitMq_Consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://nlehriei:6N6mjtRm7LXKuXMGFgfxGQ-GvZewo-fE@moose.rmq.cloudamqp.com/nlehriei");
+//Bağlantı aktifleştirme ve Kanal açma 
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+// Queque oluşturma
+channel.QueueDeclare(queue: "example-queque", exclusive: false); //consumerdaki kuyruk puslisherdaki ile birebir aynı yapılandırma da tanımlanlaıdır.
+
+//queque den mesaj okuma 
+
+EventingBasicConsumer consumer = new(channel);
+
+var consumerTag = channel.BasicConsume(queue: "example-queque", autoAck: false, consumer);
+
+///basicCncel methodu gelen bütün queque deki mesajları işlemez reddeder
+///channel.BasicCancel(consumerTag);
+
+///basicreject ile gelen tek bir mesajı reddeder
+///channel.BasicReject(deliveryTag:3,requeue:true);
+
+consumer.Received += (sender, e) =>
+{
+    //Kuyruga gelen mesajın işlendiği yerdir 
+    //e.body = kuyruktaki mesajın bütünsel olarak getirecetir
+    //e.body.Span vyea e.body.ToArray() kuyruktaki mesajı getirecekrie.
+
+
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+    //multiple : false => sadece delete edilmesini istediğim channeldaki mesajı silinmesini istiyorum
+    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+
+    ///BasicNack ile işlenmeyen mesajları geri gönderirr
+    ///requeque parametresi işlenmeyen parametrelerin kuyrukta silinip silinmeyeceğine karar vermektedir.
+    //channel.BasicNack(deliveryTag:e.DeliveryTag,multiple:false,requeue:true);
+
+
+
+};
+
+Console.Read();
---
+++ RabbitMq_Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+
+//Bağlantı Oluştuma
+factory.Uri = new("amqps://nlehriei:6N6mjtRm7LXKuXMGFgfxGQ-GvZewo-fE@moose.rmq.cloudamqp.com/nlehriei");
+
+//Bağlantı aktiflestirme ve kanal acma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Que Que oluiturma 
+//QueueDeclare : parameter : durable => mesajların kalıcığı ile ilgili parametredir.
+//QueueDeclare : parameter : exclusive=> birden fazla bağlantıyla işlem yapılıp yapılmayacağı gösteren paramtere default = true
+
+channel.QueueDeclare(queue: "example-queque", exclusive: false);
+
+///Kuyruğa mesaj iletme
+// rabbitmq kuyruğa atacağı mesajları byte türünden atar ve byte dönüştürmek gerekiyor
+//channel.BasicPublish parameter : exchange default Direct Exhangtir
+//var message = Encoding.UTF8.GetBytes("Merhaba");
+//channel.BasicPublish(exchange: "", routingKey: "example-queque", body: message);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    var message = Encoding.UTF8.GetBytes("Merhaba" + i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queque", body: message);
+}
+
+Console.Read();
