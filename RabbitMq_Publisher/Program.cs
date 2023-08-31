32324569d638424181376f8a8a6eb150601f21a7
tufan35 <34610153+tufan35@users.noreply.github.com>
2023-08-31 18:11:44 +0300

    work queue tasarımı örneklendirildi.

--- RabbitMq_Publisher/Program.cs
+++ RabbitMq_Publisher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using System.Text;
 
 ConnectionFactory factory = new();
 
@@ -7,24 +8,36 @@ factory.Uri = new("amqps://nlehriei:6N6mjtRm7LXKuXMGFgfxGQ-GvZewo-fE@moose.rmq.c
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
+string queueName = "example-work-queue";
+
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
+
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(1000);
+    var message = Encoding.UTF8.GetBytes("merhaba " + i);
+
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: queueName,
+        body: message,
+        basicProperties: properties
+        );
+}
 
 Console.Read();
 
-#region P2P Tasarımı
-
-///Bu tasarımda bir publisher ilgili mesajı direkt bir kuyruğa göndeirir ve bu mesaj kuyruğu işleyen consumer tarafından tüketilir Eğerki senaryo gereği bir mesajın bir tüketici taradınfan işlenmesi gerekiyorsa bu yaklaşım kullanılır.
-
-#endregion
-
-#region Publish/Subscribe Tasarımı
-///Bu tasarımda mesajı bir exchnage gönderir ve böylece mesaj bu exchange e bind edilmiş olan tüm kuyruklara yönlendirilir. Butasarım bir mesajın birçok tüketici tarafından işlenmesi gerektiği durumlarda kullanılır.
-#endregion
 
 #region Work Queue Tasarımı 
 
 ///Bu tasarımda publisher tarafından yayımlanmış bir mesajın birden fazla consumer arasından yalnızca birisi tarafıdnan tüketilmesi amaçlanmaktadır. Böylece mesajların işlenmesi sürecinde tüm consumerlar aynı iş yüküne ve görev dağılımına sahip olacaktır.
-#endregion
-
-#region Request/Response Tasarımı
-///Bu tasarımda publisher bir request yapar gibi kuyruğa mesaj gönderir ve bu mesajı tüketen consumerdan sonuca dair başka bir kuyruktan yanıt bekle bu tarz senaryoalar için oldukca yaygındır
+///genellikle work queue calısmalarında direct exchange paramtresi kullanılır
 #endregion
